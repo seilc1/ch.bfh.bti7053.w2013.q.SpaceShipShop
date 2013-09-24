3b7bf9fe98a60c82eb320ab1e3d3c23bb0d3542c
sc0238 <christoph.seiler@gmail.com>
2013-09-24 20:29:40 +0200

    Removed unused Files and added Bootstrap Design and dotLess.

---
+++ SpaceShipShop/Scripts/shop/display.js
@@ -0,0 +1,30 @@
+﻿var SpaceShop = SpaceShop || {};
+
+SpaceShop.Display = function() {
+
+	var heightScaler = function() {
+		$(".pillar").css("height", $(window).height());
+		$(".col-container").css("height", $(window).height() - 40);
+		$(".col-container .slider").css("margin-top", ($(window).height() - 40 - 40) / 2);
+	};
+
+	var sliderHandler = function ()
+	{
+		$(".col-container").toggleClass("collapsed");
+		
+		if ($(".col-container").hasClass("collapsed"))
+		{
+			$(".col-container .slider").html("&laquo;");
+		} else {
+			$(".col-container .slider").html("&raquo;");
+		}
+
+		console.log("sliding");
+	};
+
+	$(".slider").on("click", function() { sliderHandler(); });
+	$(window).on("resize", function () { heightScaler(); });
+	
+	heightScaler();
+	sliderHandler();
+};
