4099c6fd8e08648324230c204133fef91982aa6e
sc0238 <christoph.seiler@gmail.com>
2013-12-23 21:10:16 +0100

    Removed Console Out from EnterKey Plugin.

--- Uniques/Scripts/knockout-enterkey.js
+++ Uniques/Scripts/knockout-enterkey.js
@@ -2,9 +2,8 @@
 	init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
 		var allBindings = allBindingsAccessor();
 
-		$(element).on('keypress', function (e) {
-
-			console.log("executed");
+		$(element).on('keypress', function (e)
+		{
 			var keyCode = e.which || e.keyCode;
 			if (keyCode !== 13) {
 				return true;
