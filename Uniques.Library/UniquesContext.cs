3d31ed7b24de9ccb97472802d4e1e458e3482450
sc0238 <christoph.seiler@gmail.com>
2014-01-10 08:18:42 +0100

    SimpleSearchProvider.

---
+++ Uniques.Library/UniquesContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using StructureMap;
+using Uniques.Library.Users;
+
+namespace Uniques.Library
+{
+	public class UniquesContext
+	{
+		private const string CacheKey = "__Context";
+		private bool? _loggedIn;
+		private Data.User _currentUser;
+
+		public static UniquesContext Current
+		{
+			get
+			{
+				return HttpContext.Current.Items[CacheKey] as UniquesContext
+						?? (HttpContext.Current.Items[CacheKey] = new UniquesContext()) as UniquesContext;
+			}
+		}
+
+		public bool Authenticated
+		{
+			get { return _loggedIn.HasValue ? _loggedIn.Value : (_loggedIn = HttpContext.Current.User.Identity.IsAuthenticated).Value; }
+		}
+
+		public Data.User User
+		{
+			get { return _currentUser ?? (_currentUser = UserManager.Get(HttpContext.Current.User.Identity.Name)); }
+		}
+
+		public UserManager UserManager
+		{
+			get
+			{
+				return ObjectFactory.GetInstance<UserManager>();
+			}
+		}
+	}
+}
---
+++ Uniques.Library/Users/SimpleSearch/SimpleSearchProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniques.Library.Users.SimpleSearch
+{
+	public class SimpleSearchProvider
+	{
+		public List<string> SearchableFields
+		{
+			get { return null; }
+		}
+
+		public List<string> AllowedConditions
+		{
+			get { return new List<string>() { "And", "Or", "Equals", "Like", "GreaterThan", "SmallerThan" };}
+		}
+	}
+}
